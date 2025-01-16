fa61473486e535ac5ef70a748bcbf8ce37464cd3
KooshaYeganeh <kooshakooshadv@gmail.com>
2025-01-16 22:43:53 +0330

    config File of app

---
+++ config.py
@@ -0,0 +1,8 @@
+USERNAME = "koosha"
+KEY_PATH = "/home/koosha/.ssh/id_rsa"
+SUDO_PASSWORDS = {
+    '192.168.1.9': 'K102030k',  # Sudo password for host 192.168.1.9 (openSUSE)
+    '192.168.1.10': 'debian_password',  # Sudo password for host 192.168.1.10 (Debian)
+    '192.168.1.11': 'rocky_password',  # Sudo password for host 192.168.1.11 (Rocky)
+    # Add more hosts and their sudo passwords here
+}
